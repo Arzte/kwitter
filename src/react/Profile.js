7e0fd9f6bed8f527e05ad82f958a4bc735297fd2
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-03-13 11:57:32 -0400

    get user and current picture

--- src/react/Profile.js
+++ src/react/Profile.js
@@ -2,13 +2,58 @@ import React from "react";
 import { Menu } from "./components";
 import { userIsAuthenticated } from "./HOCs";
 
+const GET_USER_URL = "https://kwitter-api.herokuapp.com/users/test-awesome"
+const GET_USER_PHOTO = "https://kwitter-api.herokuapp.com/users/test-awesome/picture$"
+
+
+
 class Profile extends React.Component {
+  state = {
+    user: [],
+    photo: []
+  };
+  
+
+  componentDidMount() {
+    fetch(GET_USER_URL)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      this.setState({ ...this.state, user: json.user })
+    })
+  }  
+  componentDidMountAgain() {
+    fetch(GET_USER_PHOTO)
+    .then(response => response.json())
+   .then(json => {
+     this.setState({ ...this.state, photo: json })
+    })
+  }
   render() {
+    console.log(this.state)
     return (
       <>
         <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2>Profile</h2>
-      </>
+        <h2>Profile</h2> 
+        
+          <div>
+           name: {this.state.user.username}
+           pictureLocation: {this.state.user.pictureLocation}
+           displayName: {this.state.user.displayName}
+           about: {this.state.user.about}
+           googleId: {this.state.user.googleId}
+           createdAt: {this.state.user.createdAt}
+           updated: {this.state.user.updatedAt} 
+          </div>
+        
+         { this.state.photo.map(photo => (
+          <div>
+            message: {photo.message}
+
+          </div>
+         ))} 
+    </>
+    
     );
   }
 }
