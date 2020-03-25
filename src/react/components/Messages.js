6ec72224712b40d5c123cdcfcdb0a3b875ade4e6
John Wilkinson <johnpwilkinson@outlook.com>
2020-03-25 16:07:16 -0400

    fixed created at data

--- src/react/components/Messages.js
+++ src/react/components/Messages.js
@@ -7,15 +7,12 @@ import { likeMessage } from "../../redux/likes";
 import { userIsAuthenticated } from "../HOCs";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import "./Messages.css"
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import DeleteIcon from '@material-ui/icons/Delete';
-import PostMessage from './PostMessage'
+import "./Messages.css";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import DeleteIcon from "@material-ui/icons/Delete";
+import PostMessage from "./PostMessage";
 
 class Messages extends Component {
-
-
-  
   componentDidMount() {
     this.props.getMessages();
   }
@@ -29,41 +26,50 @@ class Messages extends Component {
     if (this.props.messages === null) {
       return <div></div>;
     } else {
+      function formatDate(string) {
+        var options = { year: "numeric", month: "long", day: "numeric" };
+        return new Date(string).toLocaleDateString([], options);
+      }
       return (
-        <div >
+        <div>
           <Menu isAuthenticated={this.props.isAuthenticated} />
           <div className="wrapper">
-          <Card variant="outlined" className="card">
-          <CardContent>
-          <PostMessage />
-          </CardContent>
-          </Card>
-          {this.props.messages.map(message => (
-            <Card variant="outlined" className="card" key={message.id}>
+            <Card variant="outlined" className="card">
               <CardContent>
-              <div className="message color user">{message.username}</div>
-              <div className="message color ">{message.text}</div>
-             
-              {message.likes["id"]}
-             
-              {message.username === this.props.user && (
-                <div className="trash">
-                <button onClick={e => this.handleDelete(e, message.id)}>
-                  <DeleteIcon fontSize="small" />
-                </button>
-                </div>
-              )}
-              <div className="likes">
-              <button onClick={e => this.props.likeMessage(e, message.id)}>
-                <ThumbUpIcon fontSize="small" />
-              </button>
-              <div className="message color num">{message.likes.length}</div>
-              </div>
-              <div className="message color created">Posted at: {message.createdAt}</div>
-
+                <PostMessage />
               </CardContent>
             </Card>
-          ))}
+            {this.props.messages.map(message => (
+              <Card variant="outlined" className="card" key={message.id}>
+                <CardContent>
+                  <div className="message color user">{message.username}</div>
+                  <div className="message color ">{message.text}</div>
+
+                  {message.likes["id"]}
+
+                  {message.username === this.props.user && (
+                    <div className="trash">
+                      <button onClick={e => this.handleDelete(e, message.id)}>
+                        <DeleteIcon fontSize="small" />
+                      </button>
+                    </div>
+                  )}
+                  <div className="likes">
+                    <button
+                      onClick={e => this.props.likeMessage(e, message.id)}
+                    >
+                      <ThumbUpIcon fontSize="small" />
+                    </button>
+                    <div className="message color num">
+                      {message.likes.length}
+                    </div>
+                  </div>
+                  <div className="message color created">
+                    Posted on: {formatDate(message.createdAt.slice(0, 10))}
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       );
@@ -79,9 +85,10 @@ const mapStateToProps = state => {
   };
 };
 
-
-export default userIsAuthenticated(connect(mapStateToProps, {
-  getMessages,
-  deleteMessage,
-  likeMessage
-})(Messages));
+export default userIsAuthenticated(
+  connect(mapStateToProps, {
+    getMessages,
+    deleteMessage,
+    likeMessage
+  })(Messages)
+);
