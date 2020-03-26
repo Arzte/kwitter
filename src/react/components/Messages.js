274bd47e50eb082df1eb63625facd81739c6e346
Iris Faey Hoffmeyer <hoffmeyer25@gmail.com>
2020-03-26 10:22:45 -0400

    Infinte message scrolling

--- src/react/components/Messages.js
+++ src/react/components/Messages.js
@@ -11,6 +11,7 @@ import "./Messages.css";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import DeleteIcon from "@material-ui/icons/Delete";
 import PostMessage from "./PostMessage";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 class Messages extends Component {
   componentDidMount() {
@@ -21,6 +22,10 @@ class Messages extends Component {
   };
   handleLike = (event, key) => {
     this.props.likeMessage(event, key);
+  };
+  getMoreMessages = () => {
+    let currentLength = this.props.messages.length;
+    this.props.getMessages(currentLength + 100);
   };
   render() {
     if (this.props.messages === null) {
@@ -39,37 +44,49 @@ class Messages extends Component {
                 <PostMessage />
               </CardContent>
             </Card>
-            {this.props.messages.map(message => (
-              <Card variant="outlined" className="card" key={message.id}>
-                <CardContent>
-                  <div className="message color user">{message.username}</div>
-                  <div className="message color ">{message.text}</div>
+            <InfiniteScroll
+              dataLength={this.props.messages.length} //This is important field to render the next data
+              next={this.getMoreMessages}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              {this.props.messages.map(message => (
+                <Card variant="outlined" className="card" key={message.id}>
+                  <CardContent>
+                    <div className="message color user">{message.username}</div>
+                    <div className="message color ">{message.text}</div>
 
-                  {message.likes["id"]}
+                    {message.likes["id"]}
 
-                  {message.username === this.props.user && (
-                    <div className="trash">
-                      <button onClick={e => this.handleDelete(e, message.id)}>
-                        <DeleteIcon fontSize="small" />
+                    {message.username === this.props.user && (
+                      <div className="trash">
+                        <button onClick={e => this.handleDelete(e, message.id)}>
+                          <DeleteIcon fontSize="small" />
+                        </button>
+                      </div>
+                    )}
+                    <div className="likes">
+                      <button
+                        onClick={e => this.props.likeMessage(e, message.id)}
+                      >
+                        <ThumbUpIcon fontSize="small" />
                       </button>
+                      <div className="message color num">
+                        {message.likes.length}
+                      </div>
                     </div>
-                  )}
-                  <div className="likes">
-                    <button
-                      onClick={e => this.props.likeMessage(e, message.id)}
-                    >
-                      <ThumbUpIcon fontSize="small" />
-                    </button>
-                    <div className="message color num">
-                      {message.likes.length}
+                    <div className="message color created">
+                      Posted on: {formatDate(message.createdAt.slice(0, 10))}
                     </div>
-                  </div>
-                  <div className="message color created">
-                    Posted on: {formatDate(message.createdAt.slice(0, 10))}
-                  </div>
-                </CardContent>
-              </Card>
-            ))}
+                  </CardContent>
+                </Card>
+              ))}
+            </InfiniteScroll>
           </div>
         </div>
       );
