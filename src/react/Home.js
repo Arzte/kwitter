400fa9f9fcf94ea4e775f54aa4097854346c2408
Iris Faey Hoffmeyer <hoffmeyer25@gmail.com>
2020-03-26 09:51:15 -0400

    remove excess space

--- src/react/Home.js
+++ src/react/Home.js
@@ -2,21 +2,19 @@ import React from "react";
 import { LoginForm, Menu } from "./components";
 import { userIsNotAuthenticated } from "./HOCs";
 import { Link } from "react-router-dom";
-import "./Home.css"
+import "./Home.css";
 class Home extends React.Component {
-
-  
   render() {
-
-    
-     
     return (
       <>
         <Menu />
-        <div className="font"><h2>Your favorite microblogging platform</h2></div>
+        <div className="font">
+          <h2>Your favorite microblogging platform</h2>
+        </div>
         <LoginForm />
-        <div className="font"><Link to="/register">Don't have an account? Register here</Link></div>
-       
+        <div className="font">
+          <Link to="/register">Don't have an account? Register here</Link>
+        </div>
       </>
     );
   }
